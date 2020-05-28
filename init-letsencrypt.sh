ff58f6124828b1d2d459e029aa19d69344bec6d0
Shubham Pandey <shubham9411@gmail.com>
2020-05-29 00:56:25 +0530

    Customizing scripts for used in different apps

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,22 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+if ! [ -e .env ]; then
+  echo "Please generate dotenv file first" >&2
+  exit 1
+fi
+
+SCRIPTS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+domains=$(cat .env | grep DOMAIN | awk -F "=" '{print $2}')
+port=$(cat .env | grep PORT | awk -F "=" '{print $2}')
+email=$(cat .env | grep CERTBOT_EMAIL | awk -F "=" '{print $2}')
+staging=$(cat .env | grep CERTBOT_STAGING | awk -F "=" '{print $2}')
+docker_proxy_pass=$(cat .env | grep DOCKER_PROXY_PASS | awk -F "=" '{print $2}')
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+# email="$2" # Adding a valid address is strongly recommended
+# staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -35,6 +46,12 @@ docker-compose run --rm --entrypoint "\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
+echo
+
+echo "### Creating nginx confs ..."
+for domain in "${domains[@]}"; do
+  domain=$domain proxy_pass=$docker_proxy_pass port=$port $SCRIPTS_ROOT/nginx.conf.sh > $SCRIPTS_ROOT/data/nginx/$domain.conf
+done
 echo
 
 
@@ -73,6 +90,7 @@ docker-compose run --rm --entrypoint "\
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
+    --noninteractive \
     --force-renewal" certbot
 echo
 
