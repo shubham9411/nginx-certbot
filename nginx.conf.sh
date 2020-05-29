0e1def9665dbe4f8dd067b353a8e8bb773e6f0ac
Shubham Pandey <shubham9411@gmail.com>
2020-05-29 20:10:16 +0530

    Certbot v2

--- nginx.conf.sh
+++ nginx.conf.sh
@@ -1,44 +1,39 @@
 #!/bin/sh
 
-cat <<EOF
-events { }
+cat > $file <<EOF
 
+server {
+  listen 80;
+  server_name $domain;
+  server_tokens off;
 
-http {
-  error_log /etc/nginx/error_log.log warn;
-  client_max_body_size 20m;
-
-  proxy_cache_path /etc/nginx/cache keys_zone=one:500m max_size=1000m;
-
-  server {
-      listen 80;
-      server_name localhost;
-      server_tokens off;
-
-      location / {
-        return 301 https://\$host\$request_uri;
-      }
-      
-      location /.well-known/acme-challenge/ {
-        root /var/www/certbot;
-      }
+  location / {
+    return 301 https://\$host\$request_uri;
   }
-  
-  server {
-    listen 443 ssl;
-    server_name localhost;
 
-    ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
-    include /etc/letsencrypt/options-ssl-nginx.conf;
-    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+  location /.well-known/acme-challenge/ {
+    root /var/www/certbot;
+  }
+}
 
-    location / {
-      proxy_pass http://$proxy_pass:$port;
-      proxy_set_header    Host                \$http_host;
-      proxy_set_header    X-Real-IP           \$remote_addr;
-      proxy_set_header    X-Forwarded-For     \$proxy_add_x_forwarded_for;
-    }
+server {
+  listen 443 ssl;
+  server_name $domain;
+
+  ssl_certificate /etc/letsencrypt/live/$base_domain/fullchain.pem;
+  ssl_certificate_key /etc/letsencrypt/live/$base_domain/privkey.pem;
+  include /etc/letsencrypt/options-ssl-nginx.conf;
+  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+
+  location / {
+    proxy_pass http://$proxy_pass:$port;
+    proxy_http_version 1.1;
+    proxy_set_header    Upgrade             \$http_upgrade;
+    proxy_set_header    Connection          "upgrade";
+    proxy_read_timeout  86400;
+    proxy_set_header    Host                \$http_host;
+    proxy_set_header    X-Real-IP           \$remote_addr;
+    proxy_set_header    X-Forwarded-For     \$proxy_add_x_forwarded_for;
   }
 }
 
